63fd48de28bcd5420539d010147e584395030fa6
HellersonMurilo <calabianquimurilo328@gmail.com>
2023-09-06 21:08:17 -0300

    melhoria

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
-// Variável para armazenar o número do dado
 var numeroDado = 0;
+var numeroDado1 = 0;
 
-// Função valida o número e imprime o resultado
+var contarScore = 0;
+var contarScore1 = 0;
+
 function validaGame() {
-    if (numeroDado === 1) {
-        scorePlayer0.textContent = 0;
-        pontuacaoAtual.textContent = 0;
+    if (numeroDado == 1) {
+        contarScore = 0;
         players0 = false;
+        players1 = true;
     } else {
-        var contarScore = parseInt(pontuacaoAtual.textContent) + numeroDado;
-        console.log("score:",contarScore);
+        contarScore += numeroDado;
+        console.log("score:", contarScore);
     }
 
-    if (numeroDado === 1) {
-        scorePlayer1.textContent = 0;
-        currentScore.textContent = 0;
+    if (numeroDado1 === 1) {
+        contarScore1 = 0;
         players1 = false;
+        players0 = true;
+    } else {
+        contarScore1 += numeroDado1;
+        console.log("score:", contarScore1);
     }
 }
 
-// Controle de jogadores
 var players0 = true;
 var players1 = false;
 
 function validaPlayer() {
-    var playerActive0 = document.getElementById('playerActive0');
-    var playerActive1 = document.getElementById('playerActive1');
-
     if (players0) {
-        playerActive0.style.backgroundColor = 'rgba(255, 255, 255, 0.4)';
-    } else if (playerActive0 === false) {
-        playerActive0.style.backgroundColor = '#c7365f';
-    }
-
-    if (players1) {
-        playerActive1.style.backgroundColor = 'rgba(255, 255, 255, 0.4)';
-    } else if (players1 === false) {
-        playerActive1.style.backgroundColor = '#c7365f';
+        document.getElementById('playerActive0').classList.add('player--active');
+        document.getElementById('playerActive1').classList.remove('player--active');
+    } else {
+        document.getElementById('playerActive0').classList.remove('player--active');
+        document.getElementById('playerActive1').classList.add('player--active');
     }
 }
 
-// Função reiniciar
 const btnNewGame = document.getElementById('btnNewGame');
 const scorePlayer0 = document.getElementById('score--0');
 const pontuacaoAtual = document.getElementById('current--0');
@@ -48,17 +44,48 @@ const scorePlayer1 = document.getElementById('score--1');
 const currentScore = document.getElementById('current--1');
 
 btnNewGame.addEventListener('click', () => {
+    // Zere as pontuações
     scorePlayer0.textContent = 0;
-    pontuacaoAtual.textContent = 0;
     scorePlayer1.textContent = 0;
+    pontuacaoAtual.textContent = 0;
     currentScore.textContent = 0;
+
+    // Volte ao Player 1
+    document.getElementById('playerActive0').classList.add('player--active');
+    document.getElementById('playerActive1').classList.remove('player--active');
+
+    // Defina players0 como ativo ao reiniciar o jogo
+    players0 = true;
+    players1 = false;
 });
 
-// Criando a função Rolar Dados (PRINCIPAL)
 var btnRolarDados = document.getElementById('btnRolarDados');
 
 btnRolarDados.addEventListener('click', () => {
     numeroDado = Math.floor(Math.random() * 6) + 1;
-    validaGame();
-    validaPlayer();
+    numeroDado1 = Math.floor(Math.random() * 6) + 1;
+
+    if (players0) {
+        validaGame();
+        pontuacaoAtual.textContent = contarScore;
+        validaPlayer();
+
+        if (numeroDado === 1) {
+            players0 = false;
+            players1 = true;
+            validaGame();
+            validaPlayer();
+        }
+    } else {
+        validaGame();
+        currentScore.textContent = contarScore1;
+        validaPlayer();
+
+        if (numeroDado1 === 1) {
+            players0 = true;
+            players1 = false;
+            validaGame();
+            validaPlayer();
+        }
+    }
 });
